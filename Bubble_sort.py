95ae4ec483d10b1b8ce13cb902ceb6583c615b2c
ajaybadole <49497464+ajaybadole369@users.noreply.github.com>
2020-10-08 16:31:38 +0530

    Update Bubble_sort.py
    use of space complexity and sort form of swap

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,11 +1,7 @@
 def bubbleSort(nlist):
     for passnum in range(len(nlist)-1,0,-1):
         for i in range(passnum):
-            if nlist[i]>nlist[i+1]:
-                temp = nlist[i]
-                nlist[i] = nlist[i+1]
-                nlist[i+1] = temp
-
+            if nlist[i]>nlist[i+1]:  nlist[i],nlist[i+1]=nlist[i+1],nlist[i]
 nlist = [14,46,43,27,57,41,45,21,70]
 bubbleSort(nlist)
 print(nlist)
